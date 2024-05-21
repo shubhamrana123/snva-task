1542399e60e91ce980506a8169050e6e88f2c67d
shubhamrana <shubhamrana2343@gmail.com>
2024-05-21 21:54:57 +0530

    snva task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Divider, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FirstScreen from "./container/screens/FirstScreen";
+import SecondScreen from "./container/screens/SecondScreen";
+
 
 function App() {
+  const [count1,setCount1] = useState<any>(0)
+  const [count2,setCount2] = useState<any>(0)
+  const [count3,setCount3] = useState<any>(0)
+  const [value, setValue] = React.useState('Economy');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+    setCount1(0);
+    setCount2(0);
+    setCount3(0)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+  <FirstScreen/>
+  <SecondScreen/>
+   </>
   );
 }
 
---
+++ src/container/screens/FirstScreen.tsx
@@ -0,0 +1,205 @@
+import { Button, Divider, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+const FirstScreen = () => {
+    const [count1,setCount1] = useState<any>(0)
+    const [count2,setCount2] = useState<any>(0)
+    const [count3,setCount3] = useState<any>(0)
+    const [value, setValue] = React.useState('Economy');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue((event.target as HTMLInputElement).value);
+      setCount1(0);
+      setCount2(0);
+      setCount3(0)
+    };
+  return (
+ <>
+    <Stack id='container' maxWidth={'650px'} sx={{
+
+padding:3,
+rowGap:'10px'
+}}>
+<Stack id='part-one' sx={{
+
+}}>
+<Stack sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-between',
+
+
+}}>
+<Typography> Depart From</Typography>
+<Typography> Going to</Typography>
+</Stack>
+<Stack  sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-between'
+
+}}>
+<Typography sx={{fontSize:'18px',fontWeight:'bold'}}> New Delhi</Typography>
+<Typography sx={{fontSize:'18px',fontWeight:'bold'}}> Mumbai</Typography>
+</Stack>
+<Stack  sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-between'
+
+}}>
+<Typography sx={{color:'gray'}}> DEL</Typography>
+<Typography sx={{color:'gray'}}> BOM</Typography>
+</Stack>
+</Stack>
+<Divider/>
+<Stack className="part-two" >
+<Stack  sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-between'
+
+}}>
+<Typography sx={{color:'gray'}}> Traveller class</Typography>
+<Typography sx={{color:'gray'}}> ^</Typography>
+</Stack>
+<Stack  sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-between'
+
+}}>
+<Typography sx={{color:'gray',fontWeight:'bold'}}> {count1+count2+count3} Travelers,{value}</Typography>
+
+</Stack>
+
+</Stack>
+<Divider/>
+<Stack className="part3" rowGap='5px'>
+<Stack sx={{display:'flex',
+flexDirection:'row',
+justifyContent:'space-evenly',
+
+}}>
+<Stack sx={{display:'flex',
+flexDirection:'row',
+justifyContent:'space-evenly',
+
+}}>
+<Typography sx={{fontWeight:'bold'}}>Adult</Typography>
+<Typography></Typography>
+</Stack>
+<Stack flexDirection={'row'}>
+<Typography sx={{fontWeight:'bold'}}>Adult</Typography>
+<Typography sx={{color:'gray'}}> (2-12 years)</Typography>
+</Stack>
+<Stack  flexDirection={'row'}>
+<Typography sx={{fontWeight:'bold'}}>Adult</Typography>
+<Typography  sx={{color:'gray'}}> (below 2 years)</Typography>
+</Stack>
+</Stack>
+
+<Stack sx={{
+display:'flex',
+flexDirection:'row',
+justifyContent:'space-evenly'
+}}>
+
+<Stack sx={{
+display:'flex',
+flexDirection:'row'
+}}>
+<Button variant="contained"
+onClick={()=>{
+if(count1<9){
+  setCount1(count1+1)
+}
+      
+   
+  }}>+</Button>
+<Typography sx={{textAlign:'center'}} width={'24px'}>{count1}</Typography>
+
+<Button variant="contained"
+onClick={()=>{
+if(count1>0){
+  setCount1(count1-1)
+}
+      
+   
+  }}>-</Button>
+</Stack>
+<Stack sx={{
+display:'flex',
+flexDirection:'row'
+}}>
+{/* <Typography>+</Typography> */}
+<Button variant="contained" sx={{width:'10px'}}
+onClick={()=>{
+if(count2<9){
+  setCount2(count2+1)
+}
+      
+   
+  }}>+</Button>
+<Typography sx={{textAlign:'center'}} width={'24px'}>{count2}</Typography>
+<Button variant="contained"
+onClick={()=>{
+if(count2>0){
+  setCount2(count2-1)
+}
+      
+   
+  }}>-</Button>
+</Stack>
+<Stack sx={{
+display:'flex',
+flexDirection:'row'
+}}>
+<Button variant="contained"  onClick={()=>{
+if(count3<9){
+setCount3(count3+1)
+}
+  
+
+}}>+</Button>
+<Typography sx={{textAlign:'center'}} width={'24px'}>{count3}</Typography>
+<Button variant="contained"
+onClick={()=>{
+if(count3>0){
+  setCount3(count3-1)
+}
+      
+   
+  }}>-</Button>
+</Stack>
+</Stack>
+</Stack>
+
+<Stack className="radio">
+
+<FormControl>
+  <FormLabel id="demo-radio-buttons-group-label">Flight class</FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="economy"
+    name="radio-buttons-group"
+    value={value}
+    onChange={handleChange}
+  >
+    <FormControlLabel value="Economy" control={<Radio />} label="Economy" />
+    <FormControlLabel value="Premium Economy" control={<Radio />} label="Premium Economy" />
+    <FormControlLabel value="Business" control={<Radio />} label="Business" />
+  </RadioGroup>
+</FormControl>
+</Stack>
+</Stack>
+ </>
+  )
+}
+
+export default FirstScreen
